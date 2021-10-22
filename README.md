88543f26a58ca404bd69c6ed74f3e43fb10603cc
Rachel Brindle <you@subluminal.net>
2021-10-21 18:13:27 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ArchKit
+
+A description of this package.
---
+++ Tests/ArchKitTests/ArchKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ArchKit
+
+final class ArchKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ArchKit().text, "Hello, World!")
+    }
+}
