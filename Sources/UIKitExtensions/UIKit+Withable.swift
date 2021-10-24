f7dc446ace77b50e0d54fd92fc87577e6080de7b
Rachel Brindle <you@subluminal.net>
2021-10-24 10:48:51 -0700

    Make UIKit+Withable extensions public

--- Sources/UIKitExtensions/UIKit+Withable.swift
+++ Sources/UIKitExtensions/UIKit+Withable.swift
@@ -4,18 +4,18 @@ import UIKit
 
 extension UILabel {
     @discardableResult
-    func withBodyFont() -> Self {
+    public func withBodyFont() -> Self {
         self.withFontStyle(.body)
     }
 
     @discardableResult
-    func withFontStyle(_ fontStyle: UIFont.TextStyle) -> Self {
+    public func withFontStyle(_ fontStyle: UIFont.TextStyle) -> Self {
         (self as UILabel).font = UIFont.preferredFont(forTextStyle: fontStyle)
         return self
     }
 
     @discardableResult
-    func withText(_ text: String) -> Self {
+    public func withText(_ text: String) -> Self {
         (self as UILabel).text = text
         return self
     }
@@ -23,18 +23,18 @@ extension UILabel {
 
 extension UITextField {
     @discardableResult
-    func withBodyFont() -> Self {
+    public func withBodyFont() -> Self {
         self.withFontStyle(.body)
     }
 
     @discardableResult
-    func withFontStyle(_ fontStyle: UIFont.TextStyle) -> Self {
+    public func withFontStyle(_ fontStyle: UIFont.TextStyle) -> Self {
         (self as UITextField).font = UIFont.preferredFont(forTextStyle: fontStyle)
         return self
     }
 
     @discardableResult
-    func withText(_ text: String) -> Self {
+    public func withText(_ text: String) -> Self {
         (self as UITextField).text = text
         return self
     }
@@ -42,37 +42,37 @@ extension UITextField {
 
 extension UIButton {
     @discardableResult
-    func withBodyFont() -> Self {
+    public func withBodyFont() -> Self {
         self.withFontStyle(.body)
     }
 
     @discardableResult
-    func withFontStyle(_ fontStyle: UIFont.TextStyle) -> Self {
+    public func withFontStyle(_ fontStyle: UIFont.TextStyle) -> Self {
         self.titleLabel?.font = UIFont.preferredFont(forTextStyle: fontStyle)
         return self
     }
 
     @discardableResult
-    func withTitle(_ title: String, for state: UIControl.State) -> Self {
+    public func withTitle(_ title: String, for state: UIControl.State) -> Self {
         self.setTitle(title, for: state)
         return self
     }
 
     @discardableResult
-    func withTitleColor(_ titleColor: UIColor, for state: UIControl.State) -> Self {
+    public func withTitleColor(_ titleColor: UIColor, for state: UIControl.State) -> Self {
         self.setTitleColor(titleColor, for: state)
         return self
     }
 
     @discardableResult
-    func withTitleNumberOfLines(_ lines: Int) -> Self {
+    public func withTitleNumberOfLines(_ lines: Int) -> Self {
         self.titleLabel?.numberOfLines = lines
         self.titleLabel?.lineBreakMode = .byWordWrapping
         return self
     }
 
     @discardableResult
-    func withImage(_ image: UIImage?, for state: UIControl.State) -> Self {
+    public func withImage(_ image: UIImage?, for state: UIControl.State) -> Self {
         self.setImage(image, for: state)
         return self
     }
